b4d145a2ff50ae2722869aca2e66b272765d7292
FanLubLou <jaclermont@hotmail.com>
2024-09-17 09:52:14 +0200

    commit2: architecture redux avec les fichiers store.js authSlice.js et profileSlice.js

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/api/v1';
+
+export const login = async (email, password) => {
+  try {
+    const response = await axios.post(`${API_URL}/user/login`, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error logging in', error);
+    throw error;
+  }
+};
