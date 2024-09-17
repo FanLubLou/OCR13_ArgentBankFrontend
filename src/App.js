4f9ab6cff37f6c770e182638568036dbb7b02a3d
FanLubLou <jaclermont@hotmail.com>
2024-09-17 11:07:25 +0200

    commit3 creation des composants page vides Error.jsx Home.jsx Login.jsx Profile.jsx creation des composants vides Footer.jsx Header.jsx creation du rooting dans App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Footer from "./HeaderFooter/Footer";
+import Header from "./HeaderFooter/Header";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Error from "./pages/Error/Error";
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
+    
+  )
 }
-
-export default App;
